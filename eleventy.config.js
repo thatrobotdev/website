22fe8db2790bab9983f834bc13cc570576c80539
James Kerrane <hi@jameskerrane.com>
2025-04-16 14:58:41 -0400

    Pass through pattern-pigeon to the main site!

--- eleventy.config.js
+++ eleventy.config.js
@@ -23,7 +23,8 @@ export default async function (eleventyConfig) {
 		.addPassthroughCopy({
 			"./public/": "/"
 		})
-		.addPassthroughCopy("./content/feed/pretty-atom-feed.xsl");
+		.addPassthroughCopy("./content/feed/pretty-atom-feed.xsl")
+		.addPassthroughCopy({ "./content/projects/pattern-pigeon": "projects/pattern-pigeon" });
 
 	// Run Eleventy when these files change:
 	// https://www.11ty.dev/docs/watch-serve/#add-your-own-watch-targets
